464f226486c2666808f9e0cb7e956abf44a41713
Manh TRAN <69975017+manhtranduy@users.noreply.github.com>
2025-07-06 23:53:31 +0200

    Update DownloadS3_example.py

--- DownloadS3_example.py
+++ DownloadS3_example.py
@@ -1,6 +1,5 @@
 from S3.S3_Eumetsat_Search import S3_Eumetsat_Search
 from S3.S3_Eumetsat_Download import S3_Eumetsat_Download
-# from ACprocessors import RunPOLYMER_msi
 import os
 import glob
 
@@ -10,11 +9,18 @@ products=S3_Eumetsat_Search([105.5029,107.4190,8.7030,10.9810],
                             end_date='2020-06-30')
 
 products=products.product_summary
-l2_dir='path/to/save'
+outdir='path/to/save'
+S3_Eumetsat_Download(products['product_list'],outdir=outdir)
 
+from S2.S2_Peps_Search import S2_Peps_Search
+from S2.S2_Peps_Download import S2_Peps_Download
+products=S2_Peps_Search([105.5029,107.4190,8.7030,10.9810],
+                            start_date='2020-06-01',
+                            end_date='2020-06-30')
 
-    
-S3_Eumetsat_Download(products['product_list'],outdir=l2_dir)
+products=products.product_summary
+outdir='path/to/save'
+S2_Peps_Download(products['product_list'],outdir=outdir)
 
 
 
